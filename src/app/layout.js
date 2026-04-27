b74be56bcd5b14f8f7568bd80bac4941550c03a2
Ifeoluwa Fanoiki <fanoikijoshua@outlook.com>
2026-04-27 03:17:18 +0100

    fix: layout inconsistencies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,21 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { icons } from "lucide-react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
+
+export const metadata = {
+  title: "Fotuna ",
+  description: "A dashboard for monitoring and analyzing financial signals.",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
