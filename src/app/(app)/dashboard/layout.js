b3df29179dea2f9ae335dacd3efb5a6b0e137c37
Ifeoluwa Fanoiki <fanoikijoshua@outlook.com>
2026-04-27 22:49:52 +0100

    j

--- src/app/(app)/dashboard/layout.js
+++ src/app/(app)/dashboard/layout.js
@@ -1,7 +1,26 @@
-export default function DashboardLayout({ children }) {
+import Sidebar from "@/components/layout/Sidebar";
+import Header from "@/components/layout/Header";
+
+export default function AppLayout({ children }) {
   return (
-    <div className="flex">
-      <main className="flex-1">{children}</main>
+    <div className="flex min-h-screen bg-(--surface-primary) text-(--text-primary)">
+
+      {/* SIDEBAR */}
+      <Sidebar/>
+
+      {/* MAIN AREA */}
+      <div className="flex-1 flex flex-col">
+
+        {/* HEADER */}
+        <Header/>
+
+        {/* PAGE CONTENT */}
+        <main className="flex-1 px-12 py-2">
+          {children}
+        </main>
+
+      </div>
+
     </div>
   );
 }
