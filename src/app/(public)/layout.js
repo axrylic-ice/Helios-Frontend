3ba9dc3b1e6253aa5b7f15b6548c36de8ea17135
Ifeoluwa Fanoiki <fanoikijoshua@outlook.com>
2026-05-04 13:31:09 +0100

    deploy setup

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -6,14 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="google-site-verification" content="CWo4pho8DROD76x-T8_iFsrGvgTeUO4dJV9sSKqFw1U" />
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
 
 export const metadata = {
-  title: "Fotuna ",
+  title: "Fotuna",
+  verification: {
+    google: "CWo4pho8DROD76x-T8_iFsrGvgTeUO4dJV9sSKqFw1U",
+  },
   description: "Mitigate currency volatility with predictive FX insights.",
   icons: {
     icon: "/favicon.svg",
